d324e3fc15b5eda6b83d8fc78c6d63fa753a2a5d
Leandro Parice <contato@leandroparice.com.br>
2019-05-11 17:46:00 -0300

    retirado profissional da propria lista de profissionais no dashboard

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -1,8 +1,16 @@
 const { User } = require('../models')
+const { Op } = require('sequelize')
 
 class DashboardController {
   async index (req, res) {
-    const providers = await User.findAll({ where: { provider: true } })
+    const providers = await User.findAll({
+      where: {
+        provider: true,
+        id: {
+          [Op.ne]: req.session.user.id
+        }
+      }
+    })
 
     return res.render('dashboard', { providers })
   }
